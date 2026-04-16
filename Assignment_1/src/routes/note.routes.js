4437e579594f492a473bf8cb53a03d5c0b1f1a52
DHRUVI5674 <dhruvipatel5674@gmail.com>
2026-04-16 23:04:08 +0530

    Add the get data by id route

--- Assignment_1/src/routes/note.routes.js
+++ Assignment_1/src/routes/note.routes.js
@@ -6,11 +6,13 @@ const {
   createNote,
   multipleNotes,
   getAllNotes,
+  getNotesById
 } = require('../controllers/note.controller.js');
 
 router.post('/', createNote);
 router.post('/multiple', multipleNotes);
 router.get('/',getAllNotes);
+router.get('/:id',getNotesById);
 
 
 module.exports = router;
